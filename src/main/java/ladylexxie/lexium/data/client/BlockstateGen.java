cafc7d9141c2939b61cf7f7a4e5dec51bea5c5ff
Lexxie <merideo041096@gmail.com>
2022-07-11 04:10:10 +0100

    Update BlockstateGen.java

--- src/main/java/ladylexxie/lexium/data/client/BlockstateGen.java
+++ src/main/java/ladylexxie/lexium/data/client/BlockstateGen.java
@@ -2,7 +2,10 @@ package ladylexxie.lexium.data.client;
 
 import ladylexxie.lexium.Lexium;
 import ladylexxie.lexium.blocks.LexiumBlocks;
+import net.minecraft.core.Registry;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
@@ -14,6 +17,11 @@ public class BlockstateGen extends BlockStateProvider {
 	@Override
 	protected void registerStatesAndModels() {
 		simpleBlock(LexiumBlocks.LEXIUM_ORE.get());
-		simpleBlock(LexiumBlocks.LEXIUM_BLOCK.get());
+		basicBlock(LexiumBlocks.LEXIUM_BLOCK.get());
+	}
+
+	private void basicBlock( Block block ) {
+		String name = Registry.BLOCK.getKey(block).getPath();
+		simpleBlock(LexiumBlocks.LEXIUM_BLOCK.get(), models().cubeAll(name, new ResourceLocation(Lexium.MOD_ID, "block/" + name)));
 	}
 }
