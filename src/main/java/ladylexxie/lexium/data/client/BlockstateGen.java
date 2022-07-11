66647fb4207783abe9258b8075f5e12c07c0edd9
Lexxie <merideo041096@gmail.com>
2022-07-11 03:27:18 +0100

    Update BlockstateGen.java

--- src/main/java/ladylexxie/lexium/data/client/BlockstateGen.java
+++ src/main/java/ladylexxie/lexium/data/client/BlockstateGen.java
@@ -1,3 +1,17 @@
 package ladylexxie.lexium.data.client;
 
-public class BlockstateGen { }
+import ladylexxie.lexium.Lexium;
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+public class BlockstateGen extends BlockStateProvider {
+	public BlockstateGen( DataGenerator gen, ExistingFileHelper exFileHelper ) {
+		super(gen, Lexium.MOD_ID, exFileHelper);
+	}
+
+	@Override
+	protected void registerStatesAndModels() {
+
+	}
+}
