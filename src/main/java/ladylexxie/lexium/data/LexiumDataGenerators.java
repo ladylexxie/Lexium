b2e3d183d6c712e6d1fc2c5359782ff477c67faf
Lexxie <merideo041096@gmail.com>
2022-07-11 06:01:35 +0100

    it works...

--- src/main/java/ladylexxie/lexium/data/LexiumDataGenerators.java
+++ src/main/java/ladylexxie/lexium/data/LexiumDataGenerators.java
@@ -1,7 +1,6 @@
 package ladylexxie.lexium.data;
 
 import ladylexxie.lexium.Lexium;
-import ladylexxie.lexium.data.client.BlockModelGen;
 import ladylexxie.lexium.data.client.BlockstateGen;
 import ladylexxie.lexium.data.client.ItemModelGen;
 import ladylexxie.lexium.data.client.LangGen;
@@ -19,9 +18,8 @@ public class LexiumDataGenerators {
 		Lexium.LOGGER.debug("Starting data generators...");
 		if( event.includeClient() ) {
 			generator.addProvider(new LangGen(generator));
-//			generator.addProvider(new BlockstateGen(generator, event.getExistingFileHelper()));
+			generator.addProvider(new BlockstateGen(generator, event.getExistingFileHelper()));
 			generator.addProvider(new ItemModelGen(generator, event.getExistingFileHelper()));
-			//			generator.addProvider(new BlockModelGen(generator, event.getExistingFileHelper()));
 		}
 		if( event.includeServer() ) {
 			generator.addProvider(new RecipeGen(generator));
--- src/main/java/ladylexxie/lexium/data/client/BlockstateGen.java
+++ src/main/java/ladylexxie/lexium/data/client/BlockstateGen.java
@@ -2,10 +2,7 @@ package ladylexxie.lexium.data.client;
 
 import ladylexxie.lexium.Lexium;
 import ladylexxie.lexium.blocks.LexiumBlocks;
-import net.minecraft.core.Registry;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.block.Block;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
@@ -19,11 +16,5 @@ public class BlockstateGen extends BlockStateProvider {
 		Lexium.LOGGER.debug("Generating blockstate files...");
 		simpleBlock(LexiumBlocks.LEXIUM_ORE.get());
 		simpleBlock(LexiumBlocks.LEXIUM_BLOCK.get());
-//		basicBlock(LexiumBlocks.LEXIUM_BLOCK.get());
 	}
-
-//	private void basicBlock( Block block ) {
-//		String name = Registry.BLOCK.getKey(block).getPath();
-//		simpleBlock(LexiumBlocks.LEXIUM_BLOCK.get(), models().cubeAll(name, new ResourceLocation(Lexium.MOD_ID, "block/" + name)));
-//	}
 }
